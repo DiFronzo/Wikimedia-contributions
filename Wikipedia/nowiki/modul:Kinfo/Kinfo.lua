37faf13a3a72bd40d38ba0cd28a64e4e80a4862c
Andreas Lien <30557582+DiFronzo@users.noreply.github.com>
2020-03-13 12:26:23 +0100

    Update Kinfo.lua

--- Wikipedia/nowiki/modul:Kinfo/Kinfo.lua
+++ Wikipedia/nowiki/modul:Kinfo/Kinfo.lua
@@ -1,21 +1,42 @@
-require("Modul:No globals")
+local wd = require('Module:Wd')
 local data = mw.loadData("Modul:Kinfo/data")
 local getArgs = require('Module:Arguments').getArgs
 
 local p = {}
 
-local function getRef(frame)
-	return frame:callParserFunction{ name = '#tag:ref', args = {
+local function getRef(frame, definition)
+	if definition == "landareal" or definition == "ferskvatn" then
+		return frame:callParserFunction{ name = '#tag:ref', args = {
+			'{{kilde www |url=https://www.ssb.no/statbank/table/09280/ |tittel=09280: Areal (km²), etter region, arealtype, statistikkvariabel og år |nettside=[[Statistisk sentralbyrå]] |dato=1. januar 2020 |språk=norsk bokmål}}'
+	, name = 'SSB2020'} }
+	else
+		return frame:callParserFunction{ name = '#tag:ref', args = {
     '{{kilde www |url=https://www.kartverket.no/kunnskap/Fakta-om-Norge/Arealstatistikk/Arealstatistikk-Norge/ |tittel=Arealstatistikk for Norge |nettside=[[Kartverket]] |dato=1. januar 2020 |språk=norsk bokmål}}'
 	, name = 'Kartverket2020'} }
+	end
+
+end
+
+local function tablelength(T)
+  local count = 0
+  for _ in pairs(T) do count = count + 1 end
+  return count
+end
+
+local function sumvann(num1,instans)
+	if pcall(function() return data[instans][num1]['innsjo'] end) and pcall(function() return data[instans][num1]['elvtorrfall'] end) then
+		return tonumber(data[instans][num1]['innsjo']+data[instans][num1]['elvtorrfall'])
+	else
+		error("Not a valid kommunenummer in the list")
+	end
 end
 
 local function round(num, numDecimalPlaces)
   return tonumber(string.format("%." .. (numDecimalPlaces or 0) .. "f", num))
 end
 
-local table_sort = {'kommune','totareal','areal','havflate','apentomrade','snoisbre','innsjo','elvtorrfall','myr','skog','dmark','btbygg','industri','annet','landareal'}
-local header_sort = {'Nr.','Kommune','Totalt areal','Fastland og øyer','Havflate','Åpent område','Snø, is og bre','Elv med tørrfall','Innsjø','Myr','Skog','Dyrket mark','By- og tettbebyggelse','Industriområde','Annet','Landareal'}
+local table_sort = {'kommune','totareal','areal','havflate','apentomrade','snoisbre','innsjo','elvtorrfall','myr','skog','dmark','btbygg','industri','annet','landareal','ferskvatn'}
+local header_sort = {'Nr.','Kommune','Totalt areal','Fastland og øyer','Havflate','Åpent område','Snø, is og bre','Elv med tørrfall','Innsjø','Myr','Skog','Dyrket mark','By- og tettbebyggelse','Industriområde','Annet','Landareal','Ferskvatn'}
 
 local function sorted_iter(t)
   local fin = {}
@@ -69,8 +90,8 @@ function p.tabell(frame)
 	tbl:tag('tr')
 		:addClass('sortbottom')
 		:tag('td')
-			:attr('colspan', 16)
-			:wikitext('Alle rader med unntak av "nr." og "kommune" er i enheten km². Basert på data hentet fra [[Kartverket]].' .. getRef(frame)):done()
+			:attr('colspan', tablelength(header_sort))
+			:wikitext('Alle rader med unntak av "nr." og "kommune" er i enheten km². Basert på tall hentet fra [[Kartverket]] og [[Statistisk sentralbyrå|SSB]].' .. getRef(frame) .. getRef(frame,'landareal')):done()
 --	local html = tostring(tbl)
 --	local dumphtml = require('Module:Dump')._dumphtml
 --	return dumphtml(html)
@@ -88,11 +109,31 @@ function p.innbprkm(frame)
 		if string.lower(val) == 'enhet' then
 			unit = ' <small>innb./km²</small>'
 		elseif string.lower(val) == 'ref' then
-			ref = getRef(frame)
+			ref = getRef(frame,'landareal')
+		elseif string.lower(val) == 'wd' then
+			knr = tonumber(wd._egenskaper{'rå','fremtid','nåværende','normal+','best','P2504'}) or 0
+			innb = tonumber(wd._egenskap{'rå','nåværende','P1082'}) or 0
+			if not knr or knr == '' then
+				knr = tonumber(string.match(wd._egenskaper{'rå','fremtid','nåværende','normal+','best','P300'}, "%d+")) or 0
+			end
 		end
 	end
 	
-	return  mw.language.getContentLanguage():formatNum(round(innb/tonumber(data['kommune'][knr]['landareal']),2)) .. unit .. ref
+	if pcall(function() return data['kommune'][knr]['landareal'] end) and innb then
+		return  mw.language.getContentLanguage():formatNum(round(innb/tonumber(data['kommune'][knr]['landareal']),2)) .. unit .. ref
+	elseif pcall(function() return data['fylke'][knr]['landareal'] end) and innb then
+		return mw.language.getContentLanguage():formatNum(round(innb/tonumber(data['fylke'][knr]['landareal']),2)) .. unit .. ref
+	else
+		local qid = mw.wikibase.getEntityIdForTitle( data['kommune'][knr]['kommune'] .. ' kommune',"nnwiki" )
+		knr = tonumber(wd._egenskaper{'rå','fremtid','nåværende','normal+','best',qid,'P2504'})
+		innb = tonumber(wd._egenskap{'rå','nåværende','best',qid,'P1082'})
+
+		if pcall(function() return data['kommune'][knr]['landareal'] end) and innb then
+			return  mw.language.getContentLanguage():formatNum(round(innb/tonumber(data['kommune'][knr]['landareal']),2)) .. unit .. ref
+		else
+			return ""
+		end
+	end
 end
 
 function p.main(frame)
@@ -101,6 +142,23 @@ function p.main(frame)
 	local verdi = 'kommune'
 	local unit = ''
 	local ref = ''
+	
+	for nr2, val in pairs(args) do
+		if string.lower(val) == 'enhet' then
+			unit = ' <small>km²</small>'
+		elseif string.lower(val) == 'ref' then
+			if args['hent'] then 
+				ref = getRef(frame, string.lower(args['hent']))
+			else
+				ref = getRef(frame)
+			end
+		elseif string.lower(val) == 'wd' then
+			nr = tonumber(wd._egenskaper{'rå','fremtid','nåværende','normal+','best','P2504'})
+			if not nr or nr == '' then
+				nr = tonumber(string.match(wd._egenskaper{'rå','fremtid','nåværende','normal+','best','P300'}, "%d+"))
+			end
+		end
+	end
 	
 	if nr == nil then
 		return ''
@@ -112,15 +170,10 @@ function p.main(frame)
 		verdi = 'fylke'
 	end
 	
-	for nr, val in pairs(args) do
-		if string.lower(val) == 'enhet' then
-			unit = ' <small>km²</small>'
-		elseif string.lower(val) == 'ref' then
-			ref = getRef(frame)
-		end
-	end
-	
 	if nr <= 54 and nr >= 3 then
+		if verdi == "vann" then
+			return mw.language.getContentLanguage():formatNum(tonumber(sumvann(nr, 'fylke'))) .. unit .. ref
+		end
 		if pcall(function() return data['fylke'][nr][verdi] end) then
 			if type(tonumber(data['fylke'][nr][verdi])) == "number" then
 				return mw.language.getContentLanguage():formatNum(tonumber(data['fylke'][nr][verdi])) .. unit .. ref
@@ -132,6 +185,9 @@ function p.main(frame)
 		end 
 	end
 	
+	if verdi == "vann" then
+		return mw.language.getContentLanguage():formatNum(tonumber(sumvann(nr, 'kommune'))) .. unit .. ref
+	end
 	if pcall(function() return data['kommune'][nr][verdi] end) then
 		if type(tonumber(data['kommune'][nr][verdi])) == "number" then
 			return mw.language.getContentLanguage():formatNum(tonumber(data['kommune'][nr][verdi])) .. unit .. ref
